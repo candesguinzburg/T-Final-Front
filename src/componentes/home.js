5d90d575e9b7b2185d799dc139964b4a00f978af
cande <candeguinzburg@gmail.com>
2023-12-18 10:03:23 -0300

    cambios en las credenciales de axios

--- src/componentes/home.js
+++ src/componentes/home.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Carousel } from "react-bootstrap";
 import "../css/home.css";
 
+axios.defaults.withCredentials = true
 const Home = () => {
   const [usuario, setUsuario] = useState("");
   const [comentario, setComentario] = useState("");
