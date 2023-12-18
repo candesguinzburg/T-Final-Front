27cdbc3dcab69ec26f166cfa45e7d4aa0494eccb
cande <candeguinzburg@gmail.com>
2023-12-17 21:39:51 -0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, } from "react-router-dom"
+import Ubicacion from "./componentes/ubicacion"
+import Home from "./componentes/home"
+import Deportes from "./componentes/deportes.jsx"
+import IniciarSecion from "./componentes/iniciarSecion.js"
+import Header from "./componentes/header"
+import SendMail from "./componentes/sendMail.js"
+import Registrate from "./componentes/registrate.js"
+import Error from "./componentes/error.jsx"
 
-function App() {
+
+const Nav = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+  <Header></Header>
+    <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/Home" element={<Home/>}></Route>
+    <Route path="/Deportes" element={<Deportes/>}></Route>
+    <Route path="/Ubicacion" element={<Ubicacion/>}></Route>
+    <Route path="/IniciarSecion" element={<IniciarSecion />}></Route>
+    <Route path="/sendMail" element={ <SendMail />}></Route>
+    <Route path="/registrate" element={<Registrate/>}></Route>
+    <Route path="*" element={<Error/>}></Route>
+  </Routes>
+  </div>
+  )
 }
 
-export default App;
+  
+
+export default Nav
